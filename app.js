0c3117ab6a7542500116ad1c0fe13025e0392f40
ocasta181 <ocasta181@gmail.com>
2018-09-06 12:33:15 -0700

    adding the server

---
+++ app.js
@@ -0,0 +1,25 @@
+[user]                                                                                                                                                       
+# Please adapt and uncomment the following lines:                                                                                                            
+        name = ocasta181                                                                                                                                     
+        email = ocasta181@gmail.com                                                                                                                          
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+~                                                                                                                                                            
+"~/.gitconfig" 5L, 149C written                                                                                                                              
+ocastas-mbp:slack-test-akf ocasta$ git commit -m 'add server'                                                                                                
