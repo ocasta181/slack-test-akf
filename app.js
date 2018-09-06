45e40e156f2dc3b2523e46bbee4760eb130fc43d
ocasta181 <ocasta181@gmail.com>
2018-09-06 14:40:30 -0700

    added express listen

--- app.js
+++ app.js
@@ -8,10 +8,6 @@
 // const port = process.env.PORT || 4205;
 // const router = express.Router();
 
-// // start server
-// app.listen(port, function (req, res) {
-//     console.info(`Started Express server on port ${port}`)
-// });
 
 
 /*
@@ -20,7 +16,8 @@
 */
 var slackEvents = require('slack-events-listener')('v47IxLKpDJwmULUHq7MbNOI6', onSlackEvent);
 var bodyParser = require('body-parser');
-var app = require('express')();
+const app = require('express')();
+const port = process.env.PORT || 4205;
 
 function onSlackEvent(event, cb) {
   // do something. call cb with err if you want Slack to resend the message (your database might be down)
@@ -28,5 +25,11 @@ function onSlackEvent(event, cb) {
   // writeToDatabase(event, cb);
 }
 
+
 // /slack_events should match whatever webhook you set in Slack
 app.use('/slack_events', bodyParser.json(), slackEvents);
+
+// start server
+app.listen(port, function (req, res) {
+    console.info(`Started Express server on port ${port}`)
+});
