d71a8bc4aa1670a0c45bd038b7a00eb5b4315803
ocasta181 <ocasta181@gmail.com>
2018-09-06 19:00:53 -0700

    heroku psql connection credentials

--- db.js
+++ db.js
@@ -1,18 +1,11 @@
 const { Client } = require('pg');
+
 const client = new Client({
-	host: 'ec2-107-21-126-193.compute-1.amazonaws.com',
-	port: 5432,
-	user: 'nwadcggomkyclo',
-	password: '5f90763c50039296ef6607196661dd13a8402997126e3c0706689687bcafca10',
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 
-client.connect((err) => {
-	if (err) {
-		console.error('connection error', err.stack)
-	} else {
-		console.log('connected')
-	}
-})
+client.connect();
 
 module.exports = {
 	query: (text, params, callback) => {
